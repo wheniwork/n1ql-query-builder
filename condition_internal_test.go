5ed76b0d7d9fcd83b91b7571eac8a0418612d310
Ben Getsug <ben.getsug@wheniwork.com>
2017-12-08 18:17:05 -0600

    testing

--- condition_internal_test.go
+++ condition_internal_test.go
@@ -2,6 +2,7 @@ package nqb
 
 import (
 	"bytes"
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -11,6 +12,14 @@ func TestBuildCondition_Error(t *testing.T) {
 	buf := &bytes.Buffer{}
 	err := buildCondition(buf, "", func(buf *bytes.Buffer) error {
 		return nil
+	})
+	assert.Error(t, err)
+}
+
+func TestBuildCondition_BuildFunc_Error(t *testing.T) {
+	buf := &bytes.Buffer{}
+	err := buildCondition(buf, "foo", func(buf *bytes.Buffer) error {
+		return errors.New("something went wrong")
 	})
 	assert.Error(t, err)
 }
